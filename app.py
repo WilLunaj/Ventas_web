f74cd61f52f0fdf868ef4d9740023fa1c1412507
WilLunaj <wluna19@hotmail.com>
2025-09-18 18:28:07 -0500

    fix crear tablas en render

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 LOCAL_TZ = pytz.timezone('America/Bogota')
 
 google_creds_str = os.environ.get("GOOGLE_CREDENTIALS")
